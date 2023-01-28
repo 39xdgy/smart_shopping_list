85454b8f44efa2daf619be980fe1b64cbbd40538
39xdgy <39xdgy@gmail.com>
2023-01-27 23:45:46 -0600

    remove pycache, add gitignore, finish add item basic function.

--- routes/shopping_list.py
+++ routes/shopping_list.py
@@ -5,8 +5,14 @@ from config.db import conn
 from schemas.shopping_list import shoppingListEntity, shoppingListsEntity
 from bson import ObjectId 
 
+from reader.amazon import amazon
+from reader.walmart import walmart
+
 
 shopping_list = APIRouter()
+
+amazon_reader = amazon()
+walmart_reader = walmart()
 
 @shopping_list.get('/')
 async def find_all_shopping_list():
@@ -22,8 +28,36 @@ async def create_shopping_list():
     return shoppingListEntity(conn.shopping_list.list.find_one({'_id': ObjectId(new_id.inserted_id)}))
 
 @shopping_list.put('/{id}')
-async def update_shopping_list(id, shopping_list: Shopping_list):
+async def add_item(id, link, name, priority):
+    new_shopping_list = shoppingListEntity(conn.shopping_list.list.find_one({"_id": ObjectId(id)}))
+    item_info = {}
+    if("amazon.com" in link):
+        response = amazon_reader.fetch(link)
+        if(response):
+            item_info = {
+                "full name": amazon_reader.get_name(),
+                "price": amazon_reader.get_price(),
+                "link": link,
+                "priority": int(priority)
+            }
+        else:
+            print("amazon fetch error")
+    elif("walmart.com" in link):
+        response = walmart_reader.fetch(link)
+        if(response):
+            item_info = {
+                "full name": walmart_reader.get_name(),
+                "price": walmart_reader.get_price(),
+                "link": link,
+                "priority": int(priority)
+            }
+        else:
+            print("walmart fetch error")
+    else:
+        print("ERROR: link does not meet the stander")
+
+    new_shopping_list["list"][name] = item_info
     conn.shopping_list.list.find_one_and_update({"_id": ObjectId(id)}, {
-        "$set":dict(shopping_list)
+        "$set":new_shopping_list
     })
     return shoppingListEntity(conn.shopping_list.list.find_one({"_id": ObjectId(id)}))
